feb329661f06b1789b2e9ee8ccddef877eb69408
matias123ge <33122313+matias123ge@users.noreply.github.com>
2017-11-23 18:32:12 +0100

    asdfdsaf
    dsafdsaf

---
+++ aggregate_measurements.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 18:03:08 2017
+
+@author: s174438
+"""
+
+def aggregate_measurements(tvec,data,period):
+    period=period.lower()
+    if period=="hour":
+        for i in range (len(tvec)-1):
+            if tvec[i:3]==tvec
+    elif period=="day":
+        asdfsaf
+    elif period=="month":
+        adsf
+    elif period=="hour of the day":
+        asdfadsf
+    
+    
