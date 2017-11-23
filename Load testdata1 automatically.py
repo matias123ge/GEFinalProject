70fa4dc5a21eb069e3a8c79797843b99bd8e8046
Aimas Lund <33122600+aimas-lund@users.noreply.github.com>
2017-11-23 14:10:34 +0100

    Load testdata

---
+++ Load testdata1 automatically.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 14:06:41 2017
+
+@author: s174435
+"""
+
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("testdata1.csv")
+print(data)
