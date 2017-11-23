5407965d11a620b3667473a864519e335fbb7930
Aimas Lund <33122600+aimas-lund@users.noreply.github.com>
2017-11-23 18:51:01 +0100

    More tweaks to the main script

--- mainScript.py
+++ mainScript.py
@@ -20,7 +20,8 @@ MENU LAYER 0 - Main menu
 """
 welcome = "\n============================\nWelcome!\n============================\n"
 print(welcome.center(80))
-mainMenu = np.array(["Load data","Aggregate data", "Display statistics", "Visualize electricity consumption", "Quit"]) #define options
+menuBool = False #make the user unable to do a function without loading data
+mainMenu = np.array(["Load data","Aggregate data", "Display statistics", "Visualize electricity consumption", "Show raw files", "Reset data", "Quit"]) #define options
 while True:
     for i in range(len(mainMenu)):
         print("{:d}. {:s}".format(i+1, mainMenu[i])) #print options menu
@@ -29,7 +30,7 @@ while True:
         try:
             choice = float(input("Please choose the number of a menu item: ")) #Choose in the menu. 
             if choice < 0:
-                raise ValueError #raise a valueerror if input is negative
+                raise ValueError #raise a value error if input is negative
             else:
                 pass
         except ValueError:
@@ -66,18 +67,23 @@ while True:
                     for i in range(len(fmodeMenu)):
                         print("{:d}. {:s}".format(i+1, fmodeMenu[i])) #print the fmode menu
                     try:
-                        fmodeOption = int(input("\nPlease specify the method of treatment for corrupted measurements: ")) #input fmode
+                        fmodeOption = int(input("\nPlease specify the number corresponding to the method of treatment for corrupted measurements: ")) #input fmode
+                        if choice < 0:
+                            raise ValueError #raise a value error if input is negative
+                        else:
+                            pass
                         fmode_index = np.arange(len(fmodeMenu)-1)+1
                         if np.any(fmode_index == fmodeOption): #if the user chooses one of the fmode options, execute that command
                             fmode = fmodeMenu[(fmodeOption-1)]
                             start = time.time() #make a timestamp before loading data
-                            print("\n" + fmode + ":\nOm Nom Nom...\n")
+                            print("\n============================\nfmode: '" + fmode + "':\nOm Nom Nom...\n")
                             outputLoad = load_measurements(filename, fmode)
                             stop = time.time() - start #calculate loadtime
-                            print("Data file loaded!\nLoad time: {:f} seconds.\n".format(stop)) #write loading time
+                            print("Data file loaded!\nLoad time: {:f} seconds.\n============================\n".format(stop)) #write loading time
                             databackup = np.copy(outputLoad[0])
                             tvecbackup = np.copy(outputLoad[1])
                             choice = -1
+                            menuBool = True
                             break
                         elif fmodeOption == len(fmodeMenu): #if the user chooses Return, break the while loop
                             choice = -1
@@ -87,11 +93,9 @@ while True:
                     except ValueError:
                         print("\nERROR DETECTED!\nPlease select a valid option from the menu.")
                     except SyntaxError:
-                        print("\nERROR DETECTED!\nYou have to choose something...\n")
+                        print("\nERROR DETECTED!\nThere is an issue with the SYNTAX!\nPlease use letters in the english alphabet\n")
                     except NameError:
                         print("\nERROR DETECTED!\nThe option you chose is invalid.")
-                    #except FileNotFoundError:
-                        #print("\nIt does not lookj like your file is present in the folder, chap.\nEither you did a typo or the file is not in your folder...\n*TIP* Remember the '.csv' extenstion of your filename.")
             else:
                 pass
         """
@@ -99,25 +103,64 @@ while True:
         MENU LAYER 1 - Aggregate Data
         -----------------------------
         """
-    elif choice == 2: 
-        pass #do something
+    elif choice == 2:
+        if menuBool == True:
+            print("Passed.")
+            #do something
+        else:
+            print("\nPlease load data first!\n")
+            pass
         """
         -----------------------------
         MENU LAYER 1 - Choose file name
         -----------------------------
         """
     elif choice == 3:
-        #do something else
-        pass
+        if menuBool == True:
+            print("Passed.")
+            #do something else
+        else:
+            print("\nPlease load data first!\n")
+            pass
         """
         -----------------------------
         MENU LAYER 1 - Visualize electricity consumption
         -----------------------------
         """
     elif choice == 4:
-        #do something entirely different
-        pass
-    elif choice == 5: #Exit script
+        if menuBool == True:
+            print("Passed.")
+            #do something entirely differnet
+        else:
+            print("\nPlease load data first!\n")
+            pass
+        """
+        -----------------------------
+        MENU LAYER 1 - Show raw data files
+        -----------------------------
+        """
+    elif choice == 5:
+        if menuBool == True:
+            print("Passed.")
+            #do somehting
+        else:
+            print("\nPlease load data first!\n")
+            pass
+        """
+        -----------------------------
+        MENU LAYER 1 - Reset data
+        -----------------------------
+        """
+    elif choice == 6:
+        if menuBool == True:
+            data = databackup
+            tvec = tvecbackup
+            print("\nData is reset!\n")
+            #do somehting
+        else:
+            print("\nThere is no data to reset. Please load data first!\n")
+            pass
+    elif choice == 7: #Exit script
         byebye = "\n============================\nHasta la Vista, baby\n============================"
         print(byebye.center(80))
         break
