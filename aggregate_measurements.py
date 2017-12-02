14d1b8fea0a4f49b6f3840f1845b604294a978dc
matias123ge <33122313+matias123ge@users.noreply.github.com>
2017-12-02 17:00:55 +0100

    aggregate for panda

--- aggregate_measurements.py
+++ aggregate_measurements.py
@@ -5,19 +5,19 @@ Created on Thu Nov 23 18:03:08 2017
 @author: s174438
 """
 
-def aggregate_measurements(tvec,data,period):
+import numpy as np 
+import pandas as pd
+#Convert tvec and data back to panda dataframe for easier processing:
+    header=np.array(["Year","Month","Day","Hour","Minutes","Seconds"])
+    header2=np.array(["Zone1","Zone2","Zone3","Zone4"])
+    tvec.astype(int)
+    data.astype(int)
+    tvec=pd.DataFrame(tvec,columns=header)
+    data=pd.DataFrame(data, columns=header2)
+    #Make sure period is case insensitive
     period=period.lower()
+    complete=tvec.join(data)
     if period=="hour":
-        shour=user.input("Enter a time")
-        for i in range (len(tvec)-1):
-            if tvec[:,i]==shour:
-                
-                
-    elif period=="day":
-        asdfsaf
-    elif period=="month":
-        adsf
-    elif period=="hour of the day":
-        asdfadsf
-    
+        hour:
+        sortedcomplete=pd.DataFrame.sort(complete(ascending=[3,:]))
     
