a5de72282c40b67231455cd86b4872ab9b0272ff
valdemarsoegaard <33123540+valdemarsoegaard@users.noreply.github.com>
2017-12-07 11:07:22 +0100

    Statistics... Now with comments!

--- print_statistics.py
+++ print_statistics.py
@@ -16,15 +16,16 @@ def print_statistics(data):
     OUTPUT:
         Table of data with quarters, minimum and maximum
     """
-    x=pd.DataFrame(data)
-    w=x.sum(axis=1)
-    w=pd.DataFrame(w,columns=["All ||"])
-    w=w.quantile([0,0.25,0.5,0.75,1])
-    x.columns=["1   ||","2   ||","3   ||","4   ||"]  
-    t=(x.quantile([0,0.25,0.5,0.75,1]))
-    t=(t.join(w)).T
+    x=pd.DataFrame(data) #converts data from numpy array to dataframe
+    w=x.sum(axis=1) #extra column for all zones (zones summed)
+    w=pd.DataFrame(w,columns=["All ||"]) #give it a header
+    w=w.quantile([0,0.25,0.5,0.75,1]) #find the percintiles for all zones column
+    x.columns=["1   ||","2   ||","3   ||","4   ||"]  #give a header to zone 1,2,3,4
+    t=(x.quantile([0,0.25,0.5,0.75,1])) #find the percentiles
+    t=(t.join(w)).T #join zone 1,2,3,4 and "all" and transpose it
     t.columns=([["Minimum","1. quart.","2. quart.","3. quart.","Maximum"],["========","=========","=========","=========","========="]])
-    t.index.name="Zone||"
+    #give the final dataframe (table) the correct header
+    t.index.name="Zone||" #index name for extra crispyness 
     print("===============================================================\n",t)
     
     if "period" not in globals():#no aggregation
